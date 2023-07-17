d68e62fe3bb84feea143175dadbfbf44914f9247
Alexander Jacquelline <30645201+kwaiks@users.noreply.github.com>
2023-07-17 11:05:08 +0700

    Update Shop-Context.jsx

--- src/context/Shop-Context.jsx
+++ src/context/Shop-Context.jsx
@@ -13,7 +13,11 @@ const getDefaultCart = () => {
 };
 
 const ShopContextProvider = (props) => {
+
+  // buat cartItems menjadi array
   const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  // ambil cart dari api dan set ke cartItems
   useEffect(() => {
     // axios.put("http://localhost:3000/api/carts", {
     //   items:
@@ -32,9 +36,16 @@ const ShopContextProvider = (props) => {
     return totalAmount;
   };
 
+  // ubah fungsi addToCart menjadi seperti ini, dan ubah parameter dari itemId menjadi item (lempar 1 object cart)
+  // 1. copy cartItem menjadi variable baru dengan spread array (agar terbuat array yang berbeda)
+  // 2. findIndex cartItems by _id dari parameter
+  // 3. jika ada maka cukup tambahkan qty dari element tersebut
+  // 4. jika tidak ada maka push object tsb dengan qty 1
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
   };
+
+  // kebalikan dari addToCart
   const removeCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
